bb8ce32175ef8d2a03616ab0041e4e85cece9db8
izhaojinlong <zjlcn@live.com>
2018-05-09 20:36:54 +0800

    第三次
    初稿修改  and 手机号正则表达式

--- MxOline/apps/organization/forms.py
+++ MxOline/apps/organization/forms.py
@@ -18,7 +18,7 @@ class UserAskForm(forms.ModelForm):
         正则表达式验证手机号
         """
         mobile = self.cleaned_data['mobile']
-        REGEX_MOBILE = "1[358]\d{9}$|^147\d{8}$|^176\d{8}$"
+        REGEX_MOBILE = "1[3578]\d{9}$|^147\d{8}$|^176\d{8}$"
         p = re.compile(REGEX_MOBILE)
         if p.match(mobile):
             return mobile
